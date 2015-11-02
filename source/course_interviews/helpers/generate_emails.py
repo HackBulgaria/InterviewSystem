469a6bbbbb5712579b25637a1198cdaeddf1d6bb
Pavlin Gergov <gergov.pavlin@gmail.com>
2015-11-02 16:53:08 +0200

    Add new courses emails generation

--- source/course_interviews/helpers/generate_emails.py
+++ source/course_interviews/helpers/generate_emails.py
@@ -2,11 +2,9 @@ from course_interviews.models import Student
 from post_office import mail
 
 
-class GenerateConfirmEmails:
+class GenerateNewCoursesEmails:
 
-    def __init__(self, template, confirm_interview_url, choose_interview_url):
-        self.confirm_interview_url = confirm_interview_url
-        self.choose_interview_url = choose_interview_url
+    def __init__(self, template):
         self.template = template
         self.__generated_emails = 0
         self.__errors = 0
@@ -17,7 +15,50 @@ class GenerateConfirmEmails:
     def __inc_generated_emails(self):
         self.__generated_emails += 1
 
-    def generate_emails(self):
+    def generate_new_courses_emails(self):
+        # Emails are gona be send to all applicants that applied successfully
+        recipients = [student for student in Student.objects.filter(
+            has_received_new_courses_email=False)]
+
+        for student in recipients:
+            mail.send(
+                recipients=[student.email],
+                template=self.template,
+                context={
+                    'name': student.name,
+                })
+            try:
+                self.__inc_generated_emails()
+                student.has_received_new_courses_email = True
+                student.save()
+            except Exception as e:
+                self.__inc_errors()
+                student.has_received_new_courses_email = False
+                student.save()
+                print(e)
+                pass
+
+    def get_generated_emails(self):
+        return self.__generated_emails - self.__errors
+
+    def get_errors(self):
+        return self.__errors
+
+    def get_students_with_generated_emails(self):
+        return len(Student.objects.filter(has_received_new_courses_email=True))
+
+    def get_students_without_generated_emails(self):
+        return len(Student.objects.filter(has_received_new_courses_email=False))
+
+
+class GenerateConfirmEmails(GenerateNewCoursesEmails):
+
+    def __init__(self, template, confirm_interview_url, choose_interview_url):
+        super().__init__(template)
+        self.confirm_interview_url = confirm_interview_url
+        self.choose_interview_url = choose_interview_url
+
+    def generate_confirmation_emails(self):
         # Emails are gona be send to all applicants that have
         # interview date and didn't receive an email yet
         recipients = [student for student in Student.objects.filter(
@@ -53,8 +94,8 @@ class GenerateConfirmEmails:
     def get_errors(self):
         return self.__errors
 
-    def get_students_with_generated_email(self):
+    def get_students_with_generated_emails(self):
         return len(Student.objects.filter(has_received_email=True))
 
-    def get_students_without_generated_email(self):
+    def get_students_without_generated_emails(self):
         return len(Student.objects.filter(has_received_email=False))
--- source/course_interviews/management/commands/generate_emails.py
+++ source/course_interviews/management/commands/generate_emails.py
@@ -1,24 +1,30 @@
 from django.core.management.base import BaseCommand
-from course_interviews.helpers.generate_confirm_emails import GenerateConfirmEmails
+from course_interviews.helpers.generate_emails import GenerateConfirmEmails, GenerateNewCoursesEmails
 
 
 class Command(BaseCommand):
     help = 'Generate emails for interview date confirmation'
 
     def handle(self, **options):
+        # Generate emails for the new courses
+        # template = 'new_courses'
+        # email_generator = GenerateNewCoursesEmails(template)
+        # email_generator.generate_new_courses_emails()
+
         template = 'interview_confirmation'
+        # URLs for confirming/choosing courses
         confirm_interview_url = "http://localhost:8000/confirm-interview/"
         choose_interview_url = "http://localhost:8000/choose-interview/"
 
-        confirm_email_generator = GenerateConfirmEmails(
+        email_generator = GenerateConfirmEmails(
             template, confirm_interview_url, choose_interview_url)
 
-        confirm_email_generator.generate_emails()
+        email_generator.generate_confirmation_emails()
 
-        generated_emails = confirm_email_generator.get_generated_emails()
-        errors = confirm_email_generator.get_errors()
-        students_with_emails = confirm_email_generator.get_students_with_generated_email()
-        students_without_emails = confirm_email_generator.get_students_without_generated_email()
+        generated_emails = email_generator.get_generated_emails()
+        errors = email_generator.get_errors()
+        students_with_emails = email_generator.get_students_with_generated_emails()
+        students_without_emails = email_generator.get_students_without_generated_emails()
 
         print(str(
             generated_emails) + " confirmational emails were generated, " + str(

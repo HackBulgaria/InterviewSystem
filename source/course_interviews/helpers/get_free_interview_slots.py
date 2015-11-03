d927468c5c472ffbdb9543d59a6354c41e802069
Pavlin Gergov <gergov.pavlin@gmail.com>
2015-11-03 17:21:21 +0200

    Order free slots by time

--- source/course_interviews/helpers/get_free_interview_slots.py
+++ source/course_interviews/helpers/get_free_interview_slots.py
@@ -17,7 +17,7 @@ def get_free_interview_slots():
     # If no bufers are left, use other available empty slots after the current day
     if len(available_slots) == 0:
         # The other slots are the ones that do not have students assigned to them
-        other_slots = InterviewSlot.objects.all().order_by('teacher_time_slot__date')
+        other_slots = InterviewSlot.objects.all().order_by('teacher_time_slot__date', 'start_time')
         today = datetime.now()
         available_slots = [slot for slot
                            in other_slots

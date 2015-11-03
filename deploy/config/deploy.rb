6012c56bb29c69f60f43099fa340440dfaaa21b6
Pavlin Gergov <gergov.pavlin@gmail.com>
2015-11-03 15:21:58 +0200

    Change interview system repo

--- deploy/config/deploy.rb
+++ deploy/config/deploy.rb
@@ -2,7 +2,7 @@
 lock '3.4.0'
 
 set :application, 'interview'
-set :repo_url, 'https://github.com/PavlinGergov/InterviewCommunicator.git'
+set :repo_url, 'https://github.com/HackBulgaria/InterviewSystem.git'
 
 # Default branch is :master
 ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
